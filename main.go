95c55a4f2eab41fe6160527bfccc1bbcf5986c21
Master Joy <386442876@qq.com>
2022-05-10 11:13:47 +0800

    修改端口

--- main.go
+++ main.go
@@ -7,16 +7,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main(){
+func main() {
 	route := gin.Default()
 	route.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"code": 1,
+			"code":    1,
 			"message": "pong",
 		})
 	})
 
 	time.After(1 * time.Second)
-	log.Panic(route.Run("0.0.0.0:8888"))
+	log.Panic(route.Run("0.0.0.0:8821"))
 }
-
