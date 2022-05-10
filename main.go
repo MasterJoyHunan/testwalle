b5f940ac97131023c0185dc7d88854ce49d2f2e7
Master Joy <386442876@qq.com>
2022-05-10 11:27:21 +0800

    使用 embed 打包文件

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	_ "embed"
 	"log"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
+
+//go:embed Dockerfile
+var dockerFile []byte
 
 func main() {
 	route := gin.Default()
@@ -13,6 +17,13 @@ func main() {
 		c.JSON(200, gin.H{
 			"code":    1,
 			"message": "pong",
+		})
+	})
+
+	route.GET("/getFile", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"code":    1,
+			"message": string(dockerFile),
 		})
 	})
 
